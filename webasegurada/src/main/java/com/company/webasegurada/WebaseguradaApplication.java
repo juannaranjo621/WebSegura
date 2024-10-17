a608eeffe3b897ed016ecc6330ed33e735982c08
juannaranjo621 <juangabrielnaranjo@gmail.com>
2024-10-17 17:35:59 -0500

    Aplicación web asegurada

---
+++ webasegurada/src/main/java/com/company/webasegurada/WebaseguradaApplication.java
@@ -0,0 +1,13 @@
+package com.company.webasegurada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebaseguradaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebaseguradaApplication.class, args);
+	}
+
+}
